cc7120ded1dc1611aabed96c882e741d5cc6822e
sangmaaaaan <matthew10164@gmail.com>
2024-09-25 18:22:42 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+      },
+      {
+        test: /.css?$/,
+        exclude: [],
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
