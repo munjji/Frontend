8b5ad616d8bc313f807ceda28363e3d1169a0b23
sangmaaaaan <matthew10164@gmail.com>
2024-09-28 00:09:09 +0900

    feat: add base routes and page

---
+++ src/pages/balanceGame.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BalanceGame: React.FC = () => {
+  return (
+    <div>
+      <h1>Balance Game Page</h1>
+      {/* 밸런스 게임 관련 내용 추가 */}
+    </div>
+  );
+};
+
+export default BalanceGame;
---
+++ src/pages/networkingParty.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NetworkingParty: React.FC = () => {
+  return (
+    <div>
+      <h1>Networking Party Page</h1>
+      {/* 네트워킹 파티 관련 컨텐츠 추가 */}
+    </div>
+  );
+};
+
+export default NetworkingParty;
