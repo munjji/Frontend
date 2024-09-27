37fa982c95e448c3a9f881cf22ea2031e364c537
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-27 14:42:18 +0900

    feat : Modal 컴포넌트 생성
    Modal 내부에 있는 LargeButton 커스텀을 설정해준 후 사용 가능하도록 만들었다. Modal 컴포넌트를 사용할 때 안내 텍스트만 설정해주면 된다.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,24 @@
+import { ModalProps } from 'types/Modal.type';
+import { LargeButton } from './Button';
+
+//  LargetButton에서의 Prop만 설정 후 사용 가능
+export const Modal = ({ innerText }: ModalProps) => {
+  return (
+    <div className="absolute flex flex-col items-start w-[326px] h-[200px] left-[calc(50%-163px)] top-[calc(50%-100px)] drop-shadow-[0_4px_4px_rgba(0,0,0,1)]">
+      <div className="flex flex-row justify-center items-end px-[10px] pt-[28px] w-[326px] h-[30px] bg-white border-t-2 border-x-2 border-txt_primary rounded-t-[16px]"></div>
+      <div className="flex flex-row justify-center items-center px-[10px] py-[8px] w-[326px] h-[76px] bg-white border-x-2 border-txt_primary">
+        <span className="w-[160px] h-[42px] text-sm leading-[150%] text-center text-gray-800">
+          {innerText}
+        </span>
+      </div>
+      <div className="flex flex-col justify-end items-center px-[16px] py-[16px] w-[326px] h-[95px] bg-white border-b-2 border-x-2 border-txt_primary rounded-b-[16px]">
+        <div className="flex flex-col items-center gap-[18px] w-[294px] h-[68px]">
+          <div className="flex flex-row justify-center items-start gap-[10px] w-[294px] h-[68px]">
+            <LargeButton text="아니요" bgColor="bg-white" />
+            <LargeButton text="네" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
