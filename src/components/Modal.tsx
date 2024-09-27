a30eb160e76ebac25fb97827c0b200d076bf39fb
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-27 16:09:30 +0900

    style : padding 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,7 @@ import { LargeButton } from './Button';
 //  LargetButton에서의 Prop만 설정 후 사용 가능
 export const Modal = ({ innerText }: ModalProps) => {
   return (
-    <div className="absolute flex flex-col items-start w-[326px] min-h-[184px] left-[calc(50%-163px)] top-[calc(50%-100px)] drop-shadow-[0_4px_4px_rgba(0,0,0,1)]">
+    <div className="absolute flex flex-col items-start w-[326px] min-h-[184px] left-[calc(50%-163px)] top-[calc(50%-100px)] drop-shadow-[0_4px_4px_rgba(0,0,0,1)] min-p-20px">
       <div className="flex flex-row justify-center items-end px-[10px] pt-[28px] w-[326px] h-[30px] bg-white border-t-2 border-x-2 border-txt_primary rounded-t-[16px]"></div>
       <div className="flex flex-row justify-center items-center px-[10px] py-[8px] w-[326px] h-[76px] bg-white border-x-2 border-txt_primary">
         <span className="w-[160px] h-[42px] text-sm leading-[150%] text-center text-gray-800">
--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as QuestionIcon } from '../assets/_.svg';
 
 export const Tab = () => {
   return (
-    <div className="absolute flex flex-row items-center gap-[10px] w-[325px] h-[39px] left-[92px] top-[87px] bg-#80FFC5">
+    <div className="absolute flex flex-row items-center gap-[10px] w-[375px] h-[39px] left-[92px] top-[87px] p-min-20px bg-#80FFC5">
       <XSmallButton text="처음 만났어요" />
       <XSmallButton text="시간 조율" />
       <XSmallButton text="스몰 토크" />
