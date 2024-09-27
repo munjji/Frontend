97acc65e3f39b28edd384eecc79e788482d4f96a
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-28 01:27:20 +0900

    refactor : Description & EtcBox 컴포넌트 수정
    border 색상을 txt_primary로 변경

--- src/components/ToolTips/Description.tsx
+++ src/components/ToolTips/Description.tsx
@@ -7,11 +7,11 @@ interface DescriptionProps {
 const Description: React.FC<DescriptionProps> = ({ text }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="border border-[2px] px-[16px] py-[12px] rounded-description shadow-description">
+      <div className="border border-2 border-txt_primary px-[16px] py-[12px] rounded-description shadow-description bg-white">
         <p className="text-base text-center whitespace-pre-wrap">{text}</p>
       </div>
       <img
-        className="w-[12px] h-[6px] drop-shadow-triangle"
+        className="w-[12px] h-[6px] drop-shadow-triangle z-40"
         src="/assets/triangle.svg"
         alt="triangle"
       />
