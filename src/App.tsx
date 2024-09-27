37cb17a16ba12f182c34359434c49338fb865fa3
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-27 18:20:02 +0900

    test : test 중이던 App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import './App.css';
 import SquareBox from 'components/box/SquareBox';
 
 function App() {
-  return (
-    <div className="App">
-      <ExpandedBox color="main" title="vs" isClick={false} />
-      <SquareBox color="main" content="안녕하세요 저는 이지희 입니다 하하하" question={false} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
