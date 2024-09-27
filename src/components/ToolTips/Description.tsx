0be72bc43b52165fba3d7df188f8f92098da7a19
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-28 01:16:50 +0900

    style : description 컴포넌트 bg 추가

--- src/components/ToolTips/Description.tsx
+++ src/components/ToolTips/Description.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Description: React.FC<{ text: string }> = ({ text }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="max-w-[342px] border border-txt_primary border-2 px-[16px] py-[12px] rounded-description shadow-description">
+      <div className="max-w-[342px] bg-white border border-txt_primary border-2 px-[16px] py-[12px] rounded-description shadow-description">
         <p className="text-base text-center leading-content whitespace-pre-wrap break-words">
           {text}
         </p>
