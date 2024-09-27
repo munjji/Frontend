04e039d92e09cd751d7ae02a4b67ef6283994f8e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-27 15:14:43 +0900

    style : Button 컴포넌트 최소 패팅 추가 & 반응형 버튼
    각각의 버튼의 좌우 패딩의 최솟값을 설정하기 위해 커스텀 유틸리티 클래스를 추가했다.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,11 @@ import { ButtonProps } from 'types/Button.type';
 export const LargeButton = ({ text, bgColor, textColor = 'text-black' }: ButtonProps) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-6 py-4 h-[70px] w-[132px] left-10 top-9 border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
+      className={`flex justify-center items-center gap-2 px-min-24px py-4 h-[70px] w-[132px] border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
         bgColor ? bgColor : 'bg-main_primary'
       }`}
     >
-      <span
-        className={`h-9 w-21 text-2xl leading-[140%] flex justify-center items-center ${textColor}`}
-      >
+      <span className={`text-2xl leading-[140%] flex justify-center items-center ${textColor}`}>
         {text}
       </span>
     </button>
@@ -19,13 +17,11 @@ export const LargeButton = ({ text, bgColor, textColor = 'text-black' }: ButtonP
 export const MediumButton = ({ text, bgColor, textColor = 'text-black' }: ButtonProps) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-5 py-4 h-[58px] w-[110px] left-44 top-12 border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
+      className={`flex justify-center items-center gap-2 px-min-20px py-4 h-[58px] w-[110px] border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
         bgColor ? bgColor : 'bg-main_primary'
       }`}
     >
-      <span
-        className={`h-7 w-18 text-xl leading-[140%] flex justify-center items-center ${textColor}`}
-      >
+      <span className={`text-xl leading-[140%] flex justify-center items-center ${textColor}`}>
         {text}
       </span>
     </button>
@@ -35,13 +31,11 @@ export const MediumButton = ({ text, bgColor, textColor = 'text-black' }: Button
 export const SmallButton = ({ text, bgColor, textColor = 'text-black' }: ButtonProps) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-4 py-3 h-[46px] w-[88px] left-72 top-14 border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
+      className={`flex justify-center items-center gap-2 px-min-16px py-3 h-[46px] w-[88px] border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[16px] ${
         bgColor ? bgColor : 'bg-main_primary'
       }`}
     >
-      <span
-        className={`h-6 w-14 text-lg leading-[140%] flex justify-center items-center ${textColor}`}
-      >
+      <span className={`text-lg leading-[140%] flex justify-center items-center ${textColor}`}>
         {text}
       </span>
     </button>
@@ -51,13 +45,11 @@ export const SmallButton = ({ text, bgColor, textColor = 'text-black' }: ButtonP
 export const XSmallButton = ({ text, bgColor, textColor = 'text-black' }: ButtonProps) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-3 py-2 h-[33px] w-[66px] left-96 top-16 border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[12px] ${
+      className={`flex justify-center items-center gap-2 px-min-10px py-2 h-[33px] min-w-[66px] border-txt_primary border-2 shadow-[0_4px_4px_rgba(0,0,0,1)] rounded-[12px] ${
         bgColor ? bgColor : 'bg-main_primary'
       }`}
     >
-      <span
-        className={`h-4 w-10 text-xs leading-[140%] flex justify-center items-center ${textColor}`}
-      >
+      <span className={`text-xs leading-[140%] flex justify-center items-center ${textColor}`}>
         {text}
       </span>
     </button>
