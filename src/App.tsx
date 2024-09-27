9983393852c5b14f9f03cbd54fb8654ba59448cc
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-27 14:42:48 +0900

    feat : description 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Description from 'components/ToolTips/Description';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Description
+        text="안녕하세요, 송연우입니다.
+날씨가 너무 좋네요"
+      />
     </div>
   );
 }
