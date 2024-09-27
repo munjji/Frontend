7bafb101e0a3ad9c1052a341522a2bfb8dde305f
sangmaaaaan <matthew10164@gmail.com>
2024-09-28 00:06:23 +0900

    design : add global CSS background and size

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/app-routes';
+import './index.css'; // Tailwind CSS를 사용하기 위해 추가
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-[375px] h-[812px] bg-green-200"> 
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
