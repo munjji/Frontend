53e8b9365e4f161a12db30f0b8140a29bfd25ccf
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-28 13:58:28 +0900

    feat : 랜딩 페이지 구현

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import EtcBox from 'components/box/EtcBox';
+import { LargeButton } from 'components/Button/Button';
+import onBoarding from 'data/onBoarding';
+import React from 'react';
 
 const landing = () => {
   return (
-    <div className=" text-black">landing</div>
-  )
-}
+    <div className="flex flex-col h-full justify-center items-center">
+      <div className="absolute flex flex-col justify-center items-center min-h-[639px] gap-[30px] bottom-[44px]">
+        <div className="flex flex-col justify-center items-center">
+          <img className="mb-[-70px]" src="/assets/logo.svg" alt="logo" />
+          <img
+            className="w-[360px] h-[360px] mb-[-85px]"
+            src="/assets/Char/smile.svg"
+            alt="smile"
+          />
+          <EtcBox subject="onBoarding" color="white" onBoarding={onBoarding} />
+        </div>
+        <a href="/" className="text-center text-[13px] text-txt_secondary">
+          카카오 X 구름톤 유니브 연합 해커톤 7팀
+        </a>
+        <LargeButton text="시작하기" />
+      </div>
+    </div>
+  );
+};
 
-export default landing
+export default landing;
