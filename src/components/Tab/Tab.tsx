40d3bd2539643c8549244a84cc6c1e7c740e3a9c
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-28 23:13:08 +0900

    feat : 밸런스 게임 & 스몰토크 데이터 패치

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import { XSmallButton } from '../Button/Button';
+import { TabProps } from 'types/Tab.type';
 
-export const Tab = () => {
+export const Tab = ({ buttonTexts }: TabProps) => {
+  const [active, setActive] = useState<number | null>(0);
+
+  const handleButtonClick = (index: number) => {
+    setActive(index);
+  };
   return (
-    <div className="absolute flex flex-row items-center gap-[10px] w-[375px] h-[39px] left-[92px] top-[87px] p-min-20px bg-#80FFC5">
-      <XSmallButton text="처음 만났어요" />
-      <XSmallButton text="시간 조율" />
-      <XSmallButton text="스몰 토크" />
-      <XSmallButton>
-        <img src="/assets/question-mark.svg" alt="question-mark" />
-      </XSmallButton>
+    <div className="w-full overflow-x-auto scrollbar-hidden pl-[20px] py-[10px]">
+      <div className="flex flex-row justify-start items-center gap-[10px] whitespace-nowrap">
+        {buttonTexts.map((text, index) => (
+          <XSmallButton
+            key={index}
+            text={text}
+            textColor={active === index ? 'text-black' : 'text-black'}
+            bgColor={active === index ? 'bg-main_primary' : 'bg-white'}
+            onClick={() => handleButtonClick(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
