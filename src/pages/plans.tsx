ce46d771d0773756e6249758e0daff26aab8c854
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-28 15:32:05 +0900

    feat : 플랜 둘러보기 페이지 구현

--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -1,10 +1,30 @@
+import ExpandedBox from 'components/box/ExpandedBox';
 import NavBar from 'components/NavBar';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Plans: React.FC = () => {
+  const history = useHistory();
+
+  const goToIce: React.MouseEventHandler<HTMLButtonElement> = () => {
+    history.push(`/plans/ice-braking`);
+  };
+
+  const goToSituation: React.MouseEventHandler<HTMLButtonElement> = () => {
+    history.push(`/plans/content-recommendation`);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <NavBar subject="플랜 둘러보기" nonIcon />
+      <div className="absolute flex flex-col gap-y-[16px] justify-center items-center bottom-[34px]">
+        <button type="button" onClick={goToIce}>
+          <ExpandedBox title="iceBraking" color="main" isClick={false} />
+        </button>
+        <button type="button" onClick={goToSituation}>
+          <ExpandedBox title="situation" color="main" isClick={false} />
+        </button>
+      </div>
     </div>
   );
 };
