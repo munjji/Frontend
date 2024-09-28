aca742afe9631d98fed24b6b9bfba201bae58982
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-28 15:56:54 +0900

    fix : 미니 게임 페이지 api 연결 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/app-routes';
 import './index.css'; // Tailwind CSS를 사용하기 위해 추가
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-green-200">
       <div className="w-[375px] h-[812px]">
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </QueryClientProvider>
       </div>
     </div>
   );
--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { MinigameResponse } from 'types/MinigameResponse.type';
 
 export const getMinigames = async (): Promise<MinigameResponse> => {
-  const response = await axios.get<MinigameResponse>(`/api/v1/mini-game`);
+  const response = await axios.get<MinigameResponse>(
+    `http://43.200.137.183:8080/api/v1/mini-game/`,
+  );
   return response.data;
 };
