f8167eea4a787247d6ba3d50980fd2083f59d0d8
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-28 10:43:05 +0900

    feat : 초성 퀴즈 랜덤 두 글자 초성 출력 구현

--- src/components/Cloud/RandomCloud.tsx
+++ src/components/Cloud/RandomCloud.tsx
@@ -1,13 +1,32 @@
 import { LargeButton } from 'components/Button/Button';
 import Initial from 'components/toolTips/Initial';
-import React from 'react';
+import initialConsonants from 'data/initialConstatnts';
+import React, { useState } from 'react';
 
-const RandomCloud = () => {
+const RandomCloud: React.FC = () => {
+  const [initial, setInitial] = useState<string[]>(getRandomConsonants());
+
+  function getRandomConsonants(): string[] {
+    const randomConsonants: string[] = []; // 타입 명시
+    while (randomConsonants.length < 2) {
+      const randomIndex = Math.floor(Math.random() * initialConsonants.length);
+      const consonant = initialConsonants[randomIndex];
+      if (!randomConsonants.includes(consonant)) {
+        randomConsonants.push(consonant);
+      }
+    }
+    return randomConsonants;
+  }
+
+  const handleChangeWord = () => {
+    setInitial(getRandomConsonants());
+  };
+
   return (
     <div className="absolute flex flex-col items-center justify-center bottom-[44px]">
-      <Initial />
+      <Initial initial={initial} />
       <img className="mb-[-45px]" src="/assets/Char/smile.svg" alt="smile" />
-      <LargeButton text="다른 제시어" />
+      <LargeButton text="다른 제시어" onClick={handleChangeWord} />
     </div>
   );
 };
